3f2079e6463bfec12c9342bf0f7be315f8a11149
devuterian <49933873+devuterian@users.noreply.github.com>
2026-08-04 23:00:55 +0900

    Add camera rig entrypoint

---
+++ marierie_camera_rig.jsx
@@ -0,0 +1,8 @@
+#target aftereffects
+#targetengine "marierieCameraRig"
+
+#include "./src/core.jsxinc"
+#include "./src/rig.jsxinc"
+#include "./src/expressions.jsxinc"
+#include "./src/actions.jsxinc"
+#include "./src/ui.jsxinc"
